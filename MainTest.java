c37fce684be26dca9d96b211d29fd7ed8f628219
Hatice Melike Ecevit <32355785+haticemelikeecevit@users.noreply.github.com>
2018-10-22 22:47:16 +0300

    Add files via upload

---
+++ MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testSumNumbers() {
+        assertEquals(5, Main.sumNumbers(2,3));
+    }
+}
